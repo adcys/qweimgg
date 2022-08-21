a97f154aebb3b84dd97e7259d1e8ff06854a8070
adcys <82583676+adcys@users.noreply.github.com>
2022-08-21 11:21:50 +0800

    Update month_main.js

--- month_main.js
+++ month_main.js
@@ -1,3 +1,4 @@
-document.writeln("	<a href=\'https://hm88k.com/?sheshoushiping\' target=\'_blank\'><img alt=\'2022.11.2\'");
+document.writeln("<a href=\'https://hm88k.com/?sheshoushiping\' target=\'_blank\'><img alt=\'2022.11.2\'");
 document.writeln("			src=\'https://cdn.jsdelivr.net/gh/adcys/img@main/ad/20220507-4.gif\' width=\'100%\' /></a>");
-document.writeln("	<a href=\'https://371158.com:8443/index.html?shareName=371158.com&proxyAccount=\' target=\'_blank\'><img src=\'https://cdn.jsdelivr.net/gh/adcys/qweimgg@main/b1cdf3ca8d11b7c0b5f95c8cbe5f0f86.gif\'></a>");
+document.writeln("	<a href=\'https://371158.com:8443/index.html?shareName=371158.com&proxyAccount=\' target=\'_blank\'><img");
+document.writeln("			src=\'https://cdn.jsdelivr.net/gh/adcys/qweimgg@main/b1cdf3ca8d11b7c0b5f95c8cbe5f0f86.gif\' width=\'100%\'></a>");
